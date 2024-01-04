8b43f92e0b3f457db17a87f0ed1e38e09f741491
farmerlillian <pinwenmu@gmail.com>
2024-01-04 21:06:12 +1300

    initial commit

--- Backend/6.0 DIY API/index.js
+++ Backend/6.0 DIY API/index.js
@@ -8,20 +8,70 @@ const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //1. GET a random joke
-
+app.get("/random",(req,res)=>{
+  res.json(jokes[Math.floor(Math.random()*jokes.length)])
+});
 //2. GET a specific joke
-
+app.get("/jokes/:id", (req,res)=>{
+  const id =parseInt(req.params.id);
+  const foundJoke=jokes.find((joke)=>joke.id===id);
+  res.json(foundJoke);
+});
 //3. GET a jokes by filtering on the joke type
-
+app.get("/filter",(req,res)=>{
+  const type=req.query.type.toLowerCase();
+  const filteredJokes = jokes.filter(joke => joke.jokeType.toLowerCase() === type);
+  res.json(filteredJokes);
+})
 //4. POST a new joke
-
+app.post("/jokes",(req,res)=>{
+  const newJoke={id:jokes.length+1,jokeText:req.body.text,jokeType:req.body.type};
+  jokes.push(newJoke);
+  res.json(newJoke);
+})
 //5. PUT a joke
-
+app.put("/jokes/:id", (req,res)=>{
+  const id =parseInt(req.params.id);
+  const newJoke={id:id,jokeText:req.body.text,jokeType:req.body.type};
+  const foundIndex=jokes.findIndex((joke)=>joke.id===id);
+  jokes[foundIndex]=newJoke;
+  res.json(newJoke);
+});
 //6. PATCH a joke
+app.patch("/jokes/:id", (req,res)=>{
+  const id =parseInt(req.params.id);
+  const existingJoke=jokes.find((joke)=>joke.id===id);
+  const newJoke={
+    id:id,
+    jokeText:req.body.text||existingJoke.jokeText,
+    jokeType:req.body.type||existingJoke.jokeType};
+  const foundIndex=jokes.findIndex((joke)=>joke.id===id);
+  jokes[foundIndex]=newJoke;
+  res.json(newJoke);
+});
 
 //7. DELETE Specific joke
-
+app.delete("/jokes/:id", (req,res)=>{
+  const id =parseInt(req.params.id);
+  const foundIndex=jokes.findIndex((joke)=>joke.id===id);
+  if (foundIndex>-1) {
+    jokes.splice(foundIndex,1);
+    res.sendStatus(200)
+  }
+  else{
+    res.status(404).json({error:`Joke with id: ${id} not found. No joke was deleted.`})
+  }
+});
 //8. DELETE All jokes
+app.delete("/all",(req,res)=>{
+  const userKey=req.query.key;
+  if (userKey===masterKey) {
+    jokes.splice(0,jokes.length);
+  res.sendStatus(200);
+  } else {
+    res.status(404).json({error: `You are not authorised.`})
+  }});
+
 
 app.listen(port, () => {
   console.log(`Successfully started server on port ${port}.`);
--- Backend/6.1 Blog API Project/index.js
+++ Backend/6.1 Blog API Project/index.js
@@ -41,15 +41,56 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //Write your code here//
 
 //CHALLENGE 1: GET All posts
-
+app.get("/posts",(req,res)=>{
+  console.log(posts);
+  res.json(posts);
+})
 //CHALLENGE 2: GET a specific post by id
-
+app.get("/posts/:id",(req,res)=>{
+  const id=parseInt(req.params.id);
+  const foundPost=posts.find((post)=>post.id===id);
+  if(!foundPost) return res.status(404).json({message:"Post not found"})
+  res.json(foundPost);
+})
 //CHALLENGE 3: POST a new post
-
+app.post("/posts",(req,res)=>{
+  const newPost={
+    id: posts.length+1,
+    title: req.body.title,
+    content:req.body.content,
+    author: req.body.author,
+    date: new Date().toISOString,
+  }
+  posts.push(newPost);
+  res.json(newPost)
+})
 //CHALLENGE 4: PATCH a post when you just want to update one parameter
-
+app.patch("/posts/:id",(req,res)=>{
+  const id=parseInt(req.params.id);
+  const postIndex=posts.findIndex((post)=>post.id===id);
+  const existingPost=posts.find((post)=>post.id===id);
+  if(!existingPost) return res.status(404).json({message:"Post not found"})
+  const newPost={
+    id: id,
+    title: req.body.title||existingPost.title,
+    content:req.body.content||existingPost.content,
+    author: req.body.author||existingPost.author,
+    date: new Date().toISOString,
+  }
+  posts[postIndex]=newPost;
+  res.json(newPost);
+})
 //CHALLENGE 5: DELETE a specific post by providing the post id.
-
+app.delete("/posts/:id",(req,res)=>{
+  const id=parseInt(req.params.id);
+  const postIndex=posts.findIndex((post)=>post.id===id);
+  if (postIndex>-1) {
+    posts.splice(postIndex,1);
+    res.sendStatus(200)
+    }
+  else {
+    res.status(404)
+  }})
 app.listen(port, () => {
   console.log(`API is running at http://localhost:${port}`);
 });
